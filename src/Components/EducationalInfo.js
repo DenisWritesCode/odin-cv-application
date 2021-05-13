a6ee23bff6773db886b40d672b247cdfaa55dfeb
DenisWritesCode <deniswritescode@gmail.com>
2021-05-13 09:29:38 +0200

    Delete state working

--- src/Components/EducationalInfo.js
+++ src/Components/EducationalInfo.js
@@ -1,92 +1,66 @@
 import React, { useState } from "react";
 
 function EducationalInfo() {
-  const [educationNumber, setEducationNumber] = useState(0);
-  const [education, setEducation] = useState([]);
+  const [formCount, setFormCount] = useState(0);
+  const [forms, setForms] = useState([]);
+  const formDiv = document.querySelector(".forms");
 
-  // Fix this
-  const educationInfo = document.querySelector(".educationalInfo");
-
-  function handleStudyHere(e) {
-    const study = document.querySelector("#studyHere");
-    const end = document.querySelector(".educationEndDateClass");
-
-    if (study.checked) {
-      end.classList.add("hidden");
-      console.log("Added class");
-    } else {
-      end.classList.remove("hidden");
-      console.log("Removed class");
-    }
+  function handleDelete(key) {
+    setForms(forms.filter((id) => id !== key));
   }
 
-  function handleAddEducation(educationNumber) {
-    createForm(educationNumber);
-    setEducationNumber((prevNumber) => {
-      return prevNumber + 1;
-    });
-  }
+  function addForm(count) {
+    // const form = document.createElement("FORM");
+    // form.setAttribute("id", `form${count}`);
+    // const schoolName = document.createElement("INPUT");
+    // schoolName.value = `School ${count}`;
 
-  function createForm(formNumber) {
-    const parentDiv = document.querySelector(".educationalInfo");
-    const form = document.createElement("FORM");
-    form.setAttribute("id", `education${formNumber}`);
+    // const deleteBtn = document.createElement("BUTTON");
+    // deleteBtn.innerText = "Delete";
+    // deleteBtn.type = "button";
+    // deleteBtn.onclick = function () {
+    //   handleDelete(form.id);
+    // };
 
-    form.innerHTML = `
-        <label htmlFor="school">
-          School Name
-          <input type="text" name="school" id="school" required />
-        </label>
-        <label htmlFor="study">
-          Study
-          <input type="text" name="study" id="study" required />
-        </label>
-        <label htmlFor="educationStartDate">
-          Start Date
-          <input
-            type="month"
-            name="educationStartDate"
-            id="educationStartDate"
-            required
-          />
-        </label>
+    // form.appendChild(schoolName);
+    // form.appendChild(deleteBtn);
 
-        <label htmlFor="studyHere">
-          I currently Study Here
-          <input
-            type="checkbox"
-            name="studyHere"
-            id="studyHere"
-            onClick={handleStudyHere}
-          />
-        </label>
+    setForms((forms) => forms.concat(count));
 
-        <label htmlFor="educationEndDate" className="educationEndDateClass">
-          End Date
-          <input
-            type="month"
-            name="educationEndDate"
-            id="educationEndDate"
-            className="educationEndDateClass"
-            required
-          />
-        </label>
-`;
-
-    setEducation((prevEducation) => {
-      return prevEducation.concat(form);
-    });
+    setFormCount((formCount) => formCount + 1);
   }
 
   return (
     <div className="educationalInfo">
       <h2>Educational Information</h2>
-      {education.forEach((form, index) => {
-        educationInfo.appendChild(form);
-        return 0;
-      })}
       <hr />
-      <button onClick={() => handleAddEducation(educationNumber)}>
+      <div className="forms">
+        {forms.map((formID) => {
+          return (
+            <div key={formID} className="create">
+              <form action="#">
+                <label htmlFor="school">
+                  School Name
+                  <input type="text" name="school" id="school" required />
+                </label>
+              </form>
+              <button
+                onClick={() => {
+                  handleDelete(formID);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+      <button
+        onClick={() => {
+          addForm(formCount);
+        }}
+      >
         Add Education
       </button>
     </div>
